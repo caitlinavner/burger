6b873f059102460cea9a4a287dfdba0bb98f08f1
cavner <katieavner@gmail.com>
2020-02-15 11:38:03 -0800

    db

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+DROP TABLE IF EXISTS burgers_db;
+CREATE TABLE burgers(
+  id INTEGER AUTO_INCREMENT not null,
+  burger_name VARCHAR(50),
+  devoured BOOLEAN DEFAULT false,
+  date TIMESTAMP not null DEFAULT current_timestamp,
+  primary key(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+values('Double Cheese Burger', false),
+  ('Bacon Cheese Burger', false),
+  ('Bison Burger', false);
+select
+  *
+from burgers;
